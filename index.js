c569b55ae05f24dffe09782317a8a75426dce830
Miguelangel Carvallo <67972977+Miguelangeldch@users.noreply.github.com>
2023-05-26 18:24:48 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const colors = require('colors');
-const cors = require('cors')
+const cors = require('cors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
-const connectDB = require('./config/db')
+const path = require('path');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 4000;
 
 const app = express();
@@ -12,14 +13,26 @@ const app = express();
 // Connect to database
 connectDB();
 
-app.use(cors())
+app.use(cors());
 
 app.use(
   '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: process.env.NODE_ENV === 'development', // Enable graphi enviroment to test querys
+  graphqlHTTP(async (req, res, graphQLParams) => {
+    return {
+      schema,
+      graphiql: process.env.NODE_ENV === 'development', // Enable graphi enviroment to test querys
+      context: console.log(req.headers.authorization)
+      
+    };
   })
 );
+
+// to deploy
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, console.log(`server running on ${port}`));
